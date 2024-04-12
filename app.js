22706890df8a2ddd1a16e2e4fb1b001cc870a1d3
akhilmoorkkandathil <akhildasxyz@gmail.com>
2024-04-12 11:16:52 +0530

    clear btn

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var number1 = document.getElementById("num1");
 var printButton = document.getElementById("printBtn");
 var printValue = document.getElementById("enteredNumber");
+var clearButton = document.getElementById("clearBtn");
 function printEnteredValue() {
     var numb1 = parseFloat(number1.value);
     printValue.textContent = numb1.toString();
 }
+function clearEnterdValue() {
+    printValue.textContent = "";
+}
 printButton.addEventListener('click', printEnteredValue);
+clearButton.addEventListener('click', clearEnterdValue);
--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 const number1 = document.getElementById("num1") as HTMLInputElement;
 const printButton = document.getElementById("printBtn") as HTMLButtonElement;
 const printValue = document.getElementById("enteredNumber") as HTMLOutputElement;
+const clearButton = document.getElementById("clearBtn") as HTMLButtonElement;
 
 function printEnteredValue():void {
     const numb1 = parseFloat(number1.value);
     printValue.textContent = numb1.toString();
 }
 
-printButton.addEventListener('click',printEnteredValue)
+function clearEnterdValue(): void {
+    printValue.textContent = ""
+}
+printButton.addEventListener('click',printEnteredValue);
+clearButton.addEventListener('click',clearEnterdValue)
